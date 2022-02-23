74e974400b2159a2e592b98a6a6a33f60755c265
arty91 <artem.zherdev6@gmail.com>
2022-02-23 22:26:52 +0200

    Okten Python cw2 done vr2

--- lesson2/classWork2.py
+++ lesson2/classWork2.py
@@ -36,36 +36,36 @@
 '''
 
 
-def decor():
-    count = 0
-
-    def count_check(function):
-        def increase_count():
-            nonlocal count
-            count += 1
-            print(f'Count: {count}')
-            function()
-        return increase_count
-
-    return count_check
-
-
-my_decor = decor()
-
-
-@my_decor
-def sample_func():
-    print('Function execution.')
-
-
-@my_decor
-def sample_func2():
-    print('Function execution - 2.')
-
-
-sample_func()
-sample_func()
-sample_func2()
-sample_func2()
-sample_func2()
-sample_func2()
+# def decor():
+#     count = 0
+#
+#     def count_check(function):
+#         def increase_count():
+#             nonlocal count
+#             count += 1
+#             print(f'Count: {count}')
+#             function()
+#         return increase_count
+#
+#     return count_check
+#
+#
+# my_decor = decor()
+#
+#
+# @my_decor
+# def sample_func():
+#     print('Function execution.')
+#
+#
+# @my_decor
+# def sample_func2():
+#     print('Function execution - 2.')
+#
+#
+# sample_func()
+# sample_func()
+# sample_func2()
+# sample_func2()
+# sample_func2()
+# sample_func2()
